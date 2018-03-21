fe5c3b51fa4d291764e344dc9077d579c9c14924
AndBaranov1 <37299544+AndBaranov1@users.noreply.github.com>
2018-03-21 19:17:45 +0700

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -9,30 +9,47 @@ public class Calculator {
     public static void main(String[] args) {
         //Создаем объект класса Scanner
         Scanner scanner = new Scanner(System.in);
-        //Выводим в консоль текст "Введите первое число"
-        System.out.println("Введите первое число и нажмите Enter");
-        //Cчитывает целое число с потока ввода и сохраняем в переменную "first_value"
-        double first_value = scanner.nextDouble();
-        System.out.println("Выберите операцию: +,-,/,* и нажмите Enter");
-        String operation = scanner.next();
-        System.out.println("Введите второе число и нажмите Enter");
-        //Cчитывает целое число с потока ввода и сохраняем в переменную "second_value"
-        double second_value = scanner.nextDouble();
-        /**В цикле проводим сравнение, если операция выбрана "+" то складываем значение двух переменных.
-         Аналогично и для остальных операций */
-        if (operation.equals("+")) {
-            //При выводе результат происходит форматирование до 4 знаков после запятой
-            System.out.printf("Результат: %.4f", (first_value + second_value));
-        }
-        if (operation.equals("-")) {
-            System.out.printf("Результат: %.4f", (first_value - second_value));
-        }
-        if (operation.equals("/")) {
-            System.out.printf("Результат: %.4f", (first_value / second_value));
-        }
-        if (operation.equals("*")) {
-            System.out.printf("Результат: %.4f", (first_value * second_value));
+        System.out.println("Выберите задание и нажимет Enter:(1-Calculator, 2-String array)");
+        int num = scanner.nextInt();
+        if (num == 1) {
+            //Выводим в консоль текст "Введите первое число"
+            System.out.println("Введите первое число и нажмите Enter");
+            //Cчитывает целое число с потока ввода и сохраняем в переменную "first_value"
+            double first_value = scanner.nextDouble();
+            System.out.println("Выберите операцию: +,-,/,* и нажмите Enter");
+            String operation = scanner.next();
+            System.out.println("Введите второе число и нажмите Enter");
+            //Cчитывает целое число с потока ввода и сохраняем в переменную "second_value"
+            double second_value = scanner.nextDouble();
+            /**В цикле проводим сравнение, если операция выбрана "+" то складываем значение двух переменных.
+             Аналогично и для остальных операций */
+            if (operation.equals("+")) {
+                //При выводе результат происходит форматирование до 4 знаков после запятой
+                System.out.printf("Результат: %.4f", (first_value + second_value));
+            }
+            if (operation.equals("-")) {
+                System.out.printf("Результат: %.4f", (first_value - second_value));
+            }
+            if (operation.equals("/")) {
+                System.out.printf("Результат: %.4f", (first_value / second_value));
+            }
+            if (operation.equals("*")) {
+                System.out.printf("Результат: %.4f", (first_value * second_value));
+            }
+        } else if (num == 2) {
+            System.out.println("Введите размер массива:");
+            int size = scanner.nextInt();
+            String[] array = new String[size];
+            System.out.println("Введите элементы массива:");
+            for (int i = 0; i < array.length; i++) {
+                array[i] = scanner.next();
+            }
+            String max = array[0];
+            for (int i = 0; i < array.length; i++) {
+                if (array[i].length() > max.length())
+                    max = array[i];
+            }
+            System.out.println("Результат: " + max);
         }
     }
 }
-
